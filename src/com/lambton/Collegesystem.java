d56e3b48c9c05234bcfc7b60e67d15fa57cf279c
Abhishek <abhishek.madepogu@gmail.com>
2019-05-23 19:18:57 -0400

    [Abhishek] College system class created

--- src/com/lambton/Collegesystem.java
+++ src/com/lambton/Collegesystem.java
@@ -4,6 +4,9 @@ public class Collegesystem {
     public static void main (String [] args)
     {
         System.out.println("Hello World");
-
+     for(int i=0;i<10;i++)
+     {
+         System.out.println("Abhishek");
+     }
     }
 }
